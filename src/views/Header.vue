a380af749ddf3db8a72d011888242fd45e46885d
aignatiuc <aignatiuc@pentalog.com>
2019-06-07 16:00:12 +0300

    registration => make_responsive_menu

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,44 +1,44 @@
 <template>
-  <header>
-      <b-navbar class="navbar" toggleable="lg" type="dark">
-        <b-navbar-brand to="/">
-          <img class="img" src="../../public/img/logo.png" />
-        </b-navbar-brand>
+	<header>
+		<b-navbar class="navbar" toggleable="lg" type="dark">
+			<b-navbar-brand to="/">
+				<img class="img" src="../../public/img/logo.png" />
+			</b-navbar-brand>
 
-<!--        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>-->
+			<!--        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>-->
 
-<!--        <b-collapse id="nav-collapse" is-nav>-->
+			<!--        <b-collapse id="nav-collapse" is-nav>-->
 
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown right>
-              <template slot="button-content">
-                User
-              </template>
-              <b-dropdown-item to="/profile">
-                <font-awesome-icon icon="user" />
-                &nbsp;Profile
-              </b-dropdown-item>
-              <b-dropdown-item to="/settings">
-                <font-awesome-icon icon="cog" />
-                &nbsp;Settings
-              </b-dropdown-item>
-              <b-dropdown-item to="/login">
-                <font-awesome-icon icon="power-off" />
-                &nbsp;Logout
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-<!--        </b-collapse>-->
-      </b-navbar>
-      <div class="top-bar">
-        <div class="inner">
-          <div class="top-bar-orange"></div>
-          <div class="top-bar-violet"></div>
-          <div class="top-bar-blue"></div>
-          <div class="top-bar-green"></div>
-          <div class="top-bar-bluesky"></div>
-        </div>
-      </div>
-  </header>
+			<b-navbar-nav class="ml-auto">
+				<b-nav-item-dropdown right>
+					<template slot="button-content">
+						User
+					</template>
+					<b-dropdown-item to="/profile">
+						<font-awesome-icon icon="user" />
+						&nbsp;Profile
+					</b-dropdown-item>
+					<b-dropdown-item to="/settings">
+						<font-awesome-icon icon="cog" />
+						&nbsp;Settings
+					</b-dropdown-item>
+					<b-dropdown-item to="/login">
+						<font-awesome-icon icon="power-off" />
+						&nbsp;Logout
+					</b-dropdown-item>
+				</b-nav-item-dropdown>
+			</b-navbar-nav>
+			<!--        </b-collapse>-->
+		</b-navbar>
+		<div class="top-bar">
+			<div class="inner">
+				<div class="top-bar-orange"></div>
+				<div class="top-bar-violet"></div>
+				<div class="top-bar-blue"></div>
+				<div class="top-bar-green"></div>
+				<div class="top-bar-bluesky"></div>
+			</div>
+		</div>
+	</header>
 </template>
 
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,40 +1,40 @@
 <template>
-<div class="menu-position">
-	<b-navbar toggleable="lg" type="dark" class="w-25" vertical>
-		<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+	<div>
+		<b-navbar toggleable="lg" type="dark" class="menu-position">
+			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-		<b-collapse id="nav-collapse" is-nav>
-			<b-nav vertical>
-				<b-nav-item to="/">
-					<font-awesome-icon class="fa-lg" icon="home"/>
-					&nbsp;Home
-				</b-nav-item>
-
-				<b-nav-item to="/login">
-					<font-awesome-icon class="fa-lg" icon="sign-in-alt"/>
-					&nbsp;Login /
-					<b-nav-item to="/registration">
-						Registration
+			<b-collapse id="nav-collapse" is-nav>
+				<b-nav vertical>
+					<b-nav-item class="menu-items" to="/">
+						<font-awesome-icon class="fa-lg" icon="home"/>
+						&nbsp;Home<hr>
 					</b-nav-item>
-				</b-nav-item>
 
-				<b-nav-form>
-					<b-form-input size="sm" class="mr-sm-1" placeholder="Search"></b-form-input>
-					<b-button variant="warning" size="sm" class="my-2 my-sm-0" type="submit"><font-awesome-icon class="fa-lg" icon="search"/>
-					</b-button>
-				</b-nav-form>
-			</b-nav>
-		</b-collapse>
-	</b-navbar>
-</div>
+					<b-nav-item class="menu-items" to="/login">
+						<font-awesome-icon class="fa-lg" icon="sign-in-alt"/>
+						&nbsp;Login /
+						<span to="/registration">
+						Registration<hr>
+					</span>
+					</b-nav-item>
+
+					<b-nav-form class="menu-items">
+						<b-form-input size="sm" class="mr-sm-1 col-xs-6" placeholder="Search"></b-form-input>
+						<b-button variant="warning" size="sm" class="my-2 my-sm-0" type="submit"><font-awesome-icon class="fa-lg" icon="search"/>
+						</b-button>
+					</b-nav-form>
+				</b-nav>
+			</b-collapse>
+		</b-navbar>
+	</div>
 </template>
 
 <style>
-.menu-position {
-	background: #2e2e2d;
-	/*float: left;*/
-	height: 100%;
-	width: 200px;
-	position: fixed;
-}
+	.menu-position {
+		height: 100%;
+		position: fixed;
+	}
+	.menu-items{
+		background-color: #323231;
+	}
 </style>
